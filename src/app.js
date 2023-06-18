c320614e2e6d1aa250a8f12d7bf4e6e4d44da792
cdavis1809 <133775062+cdavis1809@users.noreply.github.com>
2023-06-18 14:53:26 -0400

    Connecting weather conditions to axios

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+function displayWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let windElement = document.querySelector("#wind-speed");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  console.log(response.data);
+}
 
+let apiKey = "406035333ab93t5b036b8515eob03bf5";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=lisbon&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayWeather);
