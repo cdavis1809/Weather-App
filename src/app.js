6aa2fda7d17cc1a1091e8096b8bf3bde86177fcc
cdavis1809 <133775062+cdavis1809@users.noreply.github.com>
2023-06-19 12:41:02 -0400

    Moved HTML for forecast into JS function loop

--- src/app.js
+++ src/app.js
@@ -21,6 +21,30 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+        src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png"
+        alt=""
+        width="42px"
+        />
+        <div class="weather-forecast-temp">
+        <span class="weather-forecast-temp-high"> 18° </span>
+        <span class="weather-forecast-temp-low"> 12° </span>
+        </div>
+        </div>`;
+  });
+  forecastHTML = forecastHTML + `</div >`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
@@ -85,3 +109,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsius);
 
 search("Orlando");
+displayForecast();
